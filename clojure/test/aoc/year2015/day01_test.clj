da24b82c55a4f355546a56872a62b5606380b430
Gaelan D'costa <gdcosta@gmail.com>
2024-07-05 11:27:22 -0400

    Unit test runner, tests for 2015 day 01

---
+++ clojure/test/aoc/year2015/day01_test.clj
@@ -0,0 +1,25 @@
+(ns aoc.year2015.day01-test
+  (:require [clojure.test :refer [deftest is testing]]
+            ;; system under test
+            [aoc.year2015.day01 :as sut]))
+
+(deftest star1
+  (is (= 0 (sut/star1 "(())")))
+  (is (= 0 (sut/star1 "()()")))
+
+  (is (= 3 (sut/star1 "(((")))
+  (is (= 3 (sut/star1 "(()(()(")))
+
+  (is (= 3 (sut/star1 "))(((((")))
+
+  (is (= -1 (sut/star1 "())")))
+  (is (= -1 (sut/star1 "))(")))
+
+  (is (= -3 (sut/star1 ")))")))
+  (is (= -3 (sut/star1 ")())())"))))
+
+
+(deftest star2
+  (is (= 1 (sut/star2 ")")))
+  
+  (is (= 5 (sut/star2 "()())"))))
---
+++ clojure/test_runner.clj
@@ -0,0 +1,18 @@
+#!/usr/bin/env bb
+
+;;;; test_runner.clj -- babashka test runner from https://book.babashka.org
+
+(require '[clojure.test :as t]
+         '[babashka.classpath :as cp])
+
+;; search src/ and test/ directories for clojure file when requiring namespaces
+(cp/add-classpath "src:test")
+
+(require 'aoc.year2015.day01-test)
+
+(def test-results
+  (t/run-tests 'aoc.year2015.day01-test))
+
+(let [{:keys [fail error]} test-results]
+  (when (pos? (+ fail error))
+    (System/exit 1)))
